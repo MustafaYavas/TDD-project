6a063b48cdf5125a989c2e221a4714dab9e2340a
MustafaYavas <mustafayavas40@gmail.com>
2022-08-09 17:54:29 +0300

    Add async tests

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -2,60 +2,54 @@ import styles from './Weather.module.css';
 import LoadingSpinner from '../UI/LoadingSpinner';
 
 import { FiWind } from 'react-icons/fi';
-import { BsUmbrella } from 'react-icons/bs';
+import { BsUmbrella, BsSearch } from 'react-icons/bs';
 import { WiRaindrop } from 'react-icons/wi';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 
 const Weather = () => {
     const [city, setCity] = useState('');
     const [width, setWidth] = useState(60);
     const [weather, setWeather] = useState(null);
-    const [icon, setIcon] = useState('01d');
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(false);
 
     const inputChangeHandler = (e) => {
         setCity(e.target.value);
+
+        setWeather(null);
+    }
+
+    const searchHandler = async() => {
+        setIsLoading(true);
+        let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=b0da88335433103e03d54f032a8ed031`);
+        if(!response.ok) {
+            throw new Error('fetching employee data failed!');
+        }
+        const coords = await response.json();
+        if(coords.length === 0) {
+            setWeather(null)
+            setError(true);
+            setIsLoading(false);
+            return;
+        }
+        const lat = coords[0].lat;
+        const lon = coords[0].lon;
+        try {
+            const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b0da88335433103e03d54f032a8ed031&units=metric`);
+            setWeather(data);
+        } catch (error) {
+            setWeather(null)
+            setError(true);
+        }
+        setIsLoading(false);
     }
 
     useEffect(() => {
         if (city.length === 0) setWidth(60);
         else if(city.length <= 5) setWidth(25 + city.length * 11);
         else setWidth(10 + city.length * 12);
-
-        setIsLoading(true);
-        const fetchCoords = async () => {
-            let coords = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=b0da88335433103e03d54f032a8ed031`);
-            
-            if(coords.data.length > 0) {
-                setWeather(null);
-                setError(false)
-                let lat = coords.data[0].lat;
-                let lon = coords.data[0].lon;
-                let {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b0da88335433103e03d54f032a8ed031&units=metric`);
-
-                console.log(data);
-                setWeather(data)
-            } else {
-                setWeather(null)
-                setError(true);
-            }
-
-            // console.log(Date.parse('2022-08-06 12:00:00') / 1000);
-            setIsLoading(false);
-        }
-        
-        if(city.length > 2) {
-            const timer = setTimeout(() => {
-                fetchCoords()
-            }, 1000)
-
-            return () => {
-                clearTimeout(timer)
-            }
-        }
-
     }, [city]);
 
 
@@ -75,97 +69,88 @@ const Weather = () => {
                         maxLength='25'
                     />
                 </div>
-                <p data-testid='weather-desc' className='text-xl font-extralight'>
-                    {weather ? `it's ${weather.list[0].weather[0].main.toLowerCase()}`: ''}
-                </p>
+                {
+                    weather && 
+                    <p data-testid='weather-desc' className='text-xl font-extralight mr-2'>
+                        {`it's ${weather.list[0].weather[0].main.toLowerCase()}`}
+                    </p>
+                }
+                
+
+                <button 
+                    data-testid='search-button' 
+                    onClick={searchHandler} 
+                    className={`${styles.button}`}
+                    disabled={city.length <= 2}
+                >
+                    <span><BsSearch/></span>
+                </button>
+                
             </div>
 
             <div className={`flex flex-col justify-center items-center mt-10 pb-10 border-b-2 ${styles.container}`}>
-                {city.length <= 2 && <p data-testid='directive-parag'>Write a city you want</p>}
                 {
-                    (city.length > 2 && isLoading && !error) && 
-                    <div data-testid='loading'><LoadingSpinner /></div>
+                    city.length === 0 && 
+                    <p className='font-medium text-xl mr-4' data-testid='main-parag'>Write a city you want</p>
                 }
+                
+                    
+                    <div data-testid='loading'>
+                        {
+                            (city.length > 2 && isLoading && !error) && 
+                            <LoadingSpinner />
+                        }
+                    </div>
+                
                 {
                     (city.length > 2 && !isLoading && error) && 
-                    <p data-testid='error'>The city you were looking for was not found</p>
+                    <p className='font-medium text-xl' data-testid='error'>The city you were looking for was not found!</p>
                 }
                 {
                     (city.length > 2 && weather && !isLoading && !error) && 
                     <>
+                        <p className='font-medium text-xl'>{weather.city.name} / {weather.city.country}</p>
                         <div className={`flex justify-evenly items-center ${styles['container-items']}`}>
                             <img className={styles['img-today']} src={`${process.env.PUBLIC_URL}/assets/${weather.list[0].weather[0].icon}.svg`} alt='weather-today'/>
-                            <p className='text-7xl font-thin'>{Math.floor(weather.list[0].main.temp)}</p>
+                            <p className='text-7xl font-thin'>{Math.floor(weather.list[0].main.temp)}&#xb0;</p>
                             <div className='text-slate-400 text-2xl'>
                                 <div className='flex justify-between items-center'>
                                     <FiWind />
-                                    <p className='ml-5 '>{weather.list[0].wind.speed} <span className='text-xs'>kmh</span></p>
+                                    <p className='ml-5 '>{weather.list[0].wind.speed}<span className='text-xs'> kmh</span></p>
                                 </div>
 
                                 <div className='flex justify-between items-center'>
                                     <BsUmbrella />
-                                    <p className='ml-5'>{weather.list[0].rain ? weather.list[0].rain['3h']*100 : 0} <span className='text-xs'>%</span></p>
+                                    <p data-testid='rain' className='ml-5'>
+                                        {weather.list[0].rain ? weather.list[0].rain['3h']*100 : 0} 
+                                        <span className='text-xs'> %</span>
+                                    </p>
                                 </div>
 
                                 <div className='flex justify-between items-center'>
                                     <WiRaindrop />
-                                    <p className='ml-5'>{weather.list[0].main.humidity} <span className='text-xs'>%</span></p>
+                                    <p className='ml-5'>{weather.list[0].main.humidity}<span className='text-xs'>%</span></p>
                                 </div>
                             </div>
                         </div>
 
                         <div className={`flex justify-evenly items-center ${styles['container-items']} mt-10`}>
-                            <div className='flex flex-col justify-center items-center'>
-                                <img 
-                                    className={styles['img-future']} 
-                                    src={`${process.env.PUBLIC_URL}/assets/${icon}.svg`} 
-                                    alt='weather-future'
-                                />
-                                <div className='flex justify-center items-center'>
-                                    <p className='text-2xl font-thin'>35&#xb0;/</p>
-                                    <p className='text-2xl font-thin'>35&#xb0;</p>
-                                </div>
-                                <p className='text-lg font-thin'>TOM</p>
-                            </div>
-
-                            <div className='flex flex-col justify-center items-center'>
-                                <img 
-                                    className={styles['img-future']} 
-                                    src={`${process.env.PUBLIC_URL}/assets/${icon}.svg`} 
-                                    alt='weather-future'
-                                />
-                                <div className='flex justify-center items-center'>
-                                    <p className='text-2xl font-thin'>35&#xb0;/</p>
-                                    <p className='text-2xl font-thin'>35&#xb0;</p>
-                                </div>
-                                <p className='text-lg font-thin'>TOM</p>
-                            </div>
-
-                            <div className='flex flex-col justify-center items-center'>
-                                <img 
-                                    className={styles['img-future']} 
-                                    src={`${process.env.PUBLIC_URL}/assets/${icon}.svg`} 
-                                    alt='weather-future'
-                                />
-                                <div className='flex justify-center items-center'>
-                                    <p className='text-2xl font-thin'>35&#xb0;/</p>
-                                    <p className='text-2xl font-thin'>35&#xb0;</p>
-                                </div>
-                                <p className='text-lg font-thin'>TOM</p>
-                            </div>
-
-                            <div className='flex flex-col justify-center items-center'>
-                                <img 
-                                    className={styles['img-future']} 
-                                    src={`${process.env.PUBLIC_URL}/assets/${icon}.svg`} 
-                                    alt='weather-future'
-                                />
-                                <div className='flex justify-center items-center'>
-                                    <p className='text-2xl font-thin'>35&#xb0;/</p>
-                                    <p className='text-2xl font-thin'>35&#xb0;</p>
-                                </div>
-                                <p className='text-lg font-thin'>TOM</p>
-                            </div>
+                            {
+                                weather.list.slice(1,weather.length).map(day => (
+                                    day.dt_txt.includes('12:00:00') && 
+                                    <div key={day.dt} className='flex flex-col justify-center items-center'>
+                                        <img 
+                                            className={styles['img-future']} 
+                                            src={`${process.env.PUBLIC_URL}/assets/${day.weather[0].icon}.svg`} 
+                                            alt='weather-future'
+                                        />
+                                        <div className='flex justify-center items-center'>
+                                            <p className='text-2xl font-thin'>{Math.floor(day.main.temp)}&#xb0;</p>
+                                        </div> 
+                                        <p className='text-lg font-thin text-slate-300'>{moment(day.dt_txt, 'YYYY-MM-DD HH:mm:ss').format('dddd').substring(0,3)}</p>
+                                    </div>
+                                ))
+                            }
                         </div>
                     </>
                 }  
--- src/components/Weather/__test__/Weather.test.js
+++ src/components/Weather/__test__/Weather.test.js
@@ -19,113 +19,144 @@ jest.mock('axios', () => ({
                     country: "TR",
                     population: 313238,
                     timezone: 10800,
-                    sunrise: 1659755469,
-                    sunset: 1659805679
+                    sunrise: 1660014826,
+                    sunset: 1660064683
                 },
                 list: [
                     {
-                        dt: 1659798000,
+                        dt: 1660035600,
                         main: {
-                            temp: 26.72,
-                            feels_like: 28.61,
-                            temp_min: 25.71,
-                            temp_max: 26.72,
+                            temp: 28,
+                            feels_like: 28.69,
+                            temp_min: 28.38,
+                            temp_max: 31.62,
                             pressure: 1009,
                             sea_level: 1009,
-                            grnd_level: 1007,
-                            humidity: 73,
-                            temp_kf: 1.01
+                            grnd_level: 964,
+                            humidity: 48,
+                            temp_kf: -3.24
                         },
                         weather: [
                             {
-                                id: 803,
+                                id: 801,
                                 main: "Clouds",
-                                description: "broken clouds",
-                                icon: "04d"
+                                description: "few clouds",
+                                icon: "02d"
                             }
                         ],
                         clouds: {
-                            all: 60
+                            all: 19
                         },
                         wind: {
-                            speed: 2.62,
-                            deg: 62,
-                            gust: 3.18
+                            speed: 2.17,
+                            deg: 352,
+                            gust: 1.72
                         },
                         visibility: 10000,
-                        pop: 0.06,
+                        pop: 0,
                         sys: {
                             pod: "d"
                         },
-                        dt_txt: "2022-08-06 15:00:00"
+                        dt_txt: "2022-08-09 09:00:00"
                     }
                 ]
-        
             } 
         })
     }
 }))
 
-test('city input should be rendered', () => {
-    render(<Weather />);
-    const inputElement = screen.getByPlaceholderText(/city/i);
-    expect(inputElement).toBeInTheDocument();
-})
 
-test('city input should be empty initially', () => {
-    render(<Weather />);
-    const inputElement = screen.getByPlaceholderText(/city/i);
-    expect(inputElement.value).toHaveLength(0);
-})
-
-test('city input should change', () => {
-    render(<Weather />);
-    const inputElement = screen.getByPlaceholderText(/city/i);
-    const testValue = 'istanbul'
-    fireEvent.change(inputElement, { target: { value: testValue}})
-
-    expect(inputElement.value).not.toHaveLength(0);
-})
-
-
-// input boşken veya uzunluğu 3'den azken sadece p etiketi gösterilmeli
-test('Paragraph should be rendered when input length two or less', () => {
-    render(<Weather />);
-
-    const inputElement = screen.getByPlaceholderText(/city/i);
-    const paragraphElement = screen.getByTestId(/directive-parag/i);
-
-    const testValue = 'ab';
-
-    fireEvent.change(inputElement, { target: { value: testValue } });
-
-    expect(paragraphElement).toBeInTheDocument();
-})
-
-// verileri fetch ederken loading spinner gösterilmeli
-test('Loading spinner should be rendered while datas are fetching', async() => {
-    render(<Weather />);
-    const inputElement = screen.getByPlaceholderText(/city/i);
-
-    const testValue = 'denizli';
-
-    fireEvent.change(inputElement, { target: { value: testValue } });
-
-    const loadingElement = await screen.findByTestId(/loading/i);
-
-    expect(loadingElement).toBeInTheDocument();
-})
-
-// fetch ettikten sonra hata yoksa hava durumu açıklaması render edilmeli
-// test('Weather datas should be rendered after fetching', async() => {
-//     render(<Weather />);
-//     const inputElement = screen.getByPlaceholderText(/city/i);
-
-//     const testValue = 'denizli';
+describe('input', () => {
+    test('city input should be rendered', () => {
+        render(<Weather />);
+        const inputElement = screen.getByPlaceholderText(/city/i);
+        expect(inputElement).toBeInTheDocument();
+    })
     
-//     fireEvent.change(inputElement, { target: { value: testValue } });
-
-//     const descriptionElement = await screen.findByText(/it's clouds/i);
+    test('city input should be empty initially', () => {
+        render(<Weather />);
+        const inputElement = screen.getByPlaceholderText(/city/i);
+        expect(inputElement.value).toHaveLength(0);
+    })
     
-//     expect(descriptionElement).toBeInTheDocument();
-// })
+    test('city input should change', () => {
+        render(<Weather />);
+        const inputElement = screen.getByPlaceholderText(/city/i);
+        const testValue = 'istanbul'
+        fireEvent.change(inputElement, { target: { value: testValue}});
+        expect(inputElement.value).not.toHaveLength(0);
+    })
+})
+
+describe('button', () => {
+    test('button should not be rendered when input length is 2 or less', () => {
+        render(<Weather />);
+        const inputElement = screen.getByPlaceholderText(/city/i);
+        const testValue = 'ab';
+        fireEvent.change(inputElement, { target: { value: testValue}});
+
+        const buttonElement = screen.getByRole('button');
+        expect(buttonElement).toBeDisabled();
+    })
+})
+
+describe('weather datas', () => {
+    // fetch ettikten sonra hata yoksa hava durumu açıklaması render edilmeli
+    test('Weather datas should be rendered after fetching', async() => {
+        render(<Weather />);
+        const inputElement = screen.getByPlaceholderText(/city/i);
+        const buttonElement = screen.getByRole('button');
+
+        const testValue = 'denizli';
+        fireEvent.change(inputElement, { target: { value: testValue } });
+        fireEvent.click(buttonElement);
+
+        const descriptionElement = await screen.findByTestId(/weather-desc/i);
+        expect(descriptionElement).toBeInTheDocument();
+        
+    })
+})
+
+describe('other components', () => {
+    test('Main paragraph should be rendered only input is not empty', async() => {
+        render(<Weather />);
+        const inputElement = screen.getByPlaceholderText(/city/i);
+        const buttonElement = screen.getByRole('button');
+
+        const testValue = '';
+        fireEvent.change(inputElement, { target: { value: testValue } });
+        fireEvent.click(buttonElement);
+
+        const paragraphElement = await screen.findByTestId(/main-parag/i);
+        expect(paragraphElement).toBeInTheDocument();
+    })
+
+    test('Loading spinner should be rendered while fetching', async() => {
+        render(<Weather />);
+        const inputElement = screen.getByPlaceholderText(/city/i);
+        const buttonElement = screen.getByRole('button');
+
+        const testValue = 'denizli';
+        fireEvent.change(inputElement, { target: { value: testValue } });
+        fireEvent.click(buttonElement);
+
+        const loadingElement = await screen.findByTestId(/loading/i);
+        expect(loadingElement).toBeInTheDocument();
+    })
+
+
+    test('Loading spinner should NOT be rendered after fetching', async() => {
+        render(<Weather />);
+        const inputElement = screen.getByPlaceholderText(/city/i);
+        const buttonElement = screen.getByRole('button');
+
+        const testValue = 'denizli';
+        fireEvent.change(inputElement, { target: { value: testValue } });
+        fireEvent.click(buttonElement);
+
+        await waitFor(async() => {
+            const loadingElement = await screen.findByTestId(/loading/i);
+            expect(loadingElement.value).toBe(undefined);
+        })
+    })
+})
